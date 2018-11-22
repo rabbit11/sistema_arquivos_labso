48e7b91353033f9c51ae11bb134a252acac45520
Pietro <Pietro@localhost.localdomain>
2018-11-22 11:05:51 -0200

    Inicio do trab

---
+++ rsfs/fs.c
@@ -0,0 +1,94 @@
+/*
+ * RSFS - Really Simple File System
+ *
+ * Copyright © 2010 Gustavo Maciel Dias Vieira
+ * Copyright © 2010 Rodrigo Rocco Barbieri
+ *
+ * This file is part of RSFS.
+ *
+ * RSFS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "disk.h"
+#include "fs.h"
+
+#define CLUSTERSIZE 4096
+
+unsigned short fat[65536];			/* FAT */
+
+typedef struct {
+	char used;						/* booleano que indica se a entrada está em uso */
+	char name[25];					/* nome do arquivo */
+	unsigned short first_block;		/* primeiro agrupamento do arquivo */
+	int size;						/* tamanho do arquivo em bytes */
+} dir_entry; // 32 bytes
+
+dir_entry dir[128];					/* diretório */
+
+/* carrega a fat e o diretório */
+int fs_init() {
+	printf("Função não implementada: fs_init\n");
+	return 1;
+}
+
+/* cria novas estruturas na memória e as escreve no disco */
+int fs_format() {
+	printf("Função não implementada: fs_format\n");
+	return 0;
+}
+
+/* pega a fat e varre todos os campos marcados como 1 (livres) */
+int fs_free() {
+	printf("Função não implementada: fs_free\n");
+	return 0;
+}
+
+/* pega o diretorio, varre as 128 entradas e concatena no buffer */
+int fs_list(char *buffer, int size) {
+	printf("Função não implementada: fs_list\n");
+	return 0;
+}
+
+int fs_create(char *file_name) {
+	printf("Função não implementada: fs_create\n");
+	return 0;
+}
+
+int fs_remove(char *file_name) {
+	printf("Função não implementada: fs_remove\n");
+	return 0;
+}
+
+int fs_open(char *file_name, int mode) {
+	printf("Função não implementada: fs_open\n");
+	return -1;
+}
+
+int fs_close(int file) {
+	printf("Função não implementada: fs_close\n");
+	return 0;
+}
+
+int fs_write(char *buffer, int size, int file) {
+	printf("Função não implementada: fs_write\n");
+	return -1;
+}
+
+int fs_read(char *buffer, int size, int file) {
+	printf("Função não implementada: fs_read\n");
+	return -1;
+}
